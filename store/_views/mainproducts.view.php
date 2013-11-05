60e695274adff37c0a81c3e2477e79e4e87afa81
MichaelCStevens <info@michaelstevensdesign.com>
2013-11-04 22:41:45 -0500

    Update mainproducts.view.php

--- store/_views/mainproducts.view.php
+++ store/_views/mainproducts.view.php
@@ -9,68 +9,12 @@ foreach ($objStore->objModel->getProducts() as $product) {
     $subs = $objStore->objModel->getSubProducts($product['entity_id']);
     $formattedTitle = explode('(', $product['title']);
     $formattedTitle = $formattedTitle[0];
-    //crapy hacks thanks for changing spec two mins before launch, client
-    $stringVar = $formattedTitle;
-    if($product[3] == 40){
-        $marketPhrase = 'The First Choice for Kansas City';
-        $altPhrase = 'The First Choice for Kansas City';
-        $titlePhrase = 'The First Choice for Kansas City';
-        $productTitle = 'Kansas City Strip Steaks';
-    }
-    elseif($product[3] == 42){
-        $marketPhrase = 'The Tender Best!';
-        $altPhrase = 'The Tender Best!';
-        $titlePhrase = 'The Tender Best!';
-        $productTitle = 'Filet Mignon';
-    }
-    elseif($product[3] == 43){
-        $marketPhrase = 'Choose Racks, Chops or Roasts';
-        $altPhrase = 'Choose Racks, Chops or Roasts';
-        $titlePhrase = 'Choose Racks, Chops or Roasts';
-        $productTitle = 'Lamb';
-    }
-    elseif($product[3] == 46){
-        $marketPhrase = 'Combo Packs of your favorite Steaks';
-        $altPhrase = 'Combo Packs of your favorite Steaks';
-        $titlePhrase = 'Combo Packs of your favorite Steaks';
-        $productTitle = 'Mix and Match';
-    }
-    elseif($product[3] == 49){
-        $marketPhrase = 'Best of Both Worlds';
-        $altPhrase = 'Best of Both Worlds';
-        $titlePhrase = 'Best of Both Worlds';
-        $productTitle = 'Porterhouse';
-    }
-    elseif($product[3] == 60){
-        $marketPhrase = "The Butcher's Choice";
-        $altPhrase = "The Butcher's Choice";
-        $titlePhrase = "The Butcher's Choice";
-        $productTitle = 'Ribeye';
-    }
-    elseif($product[3] == 69){
-        $marketPhrase = 'Choose Chops or Racks';
-        $altPhrase = 'Choose Chops or Racks';
-        $titlePhrase = 'Choose Chops or Racks';
-        $productTitle = 'Berkshire Pork';
-    }
-    elseif($product[3] == 70){
-        $marketPhrase = 'Roast with the Most!';
-        $altPhrase = 'Roast with the Most!';
-        $titlePhrase = 'Roast with the Most!';
-        $productTitle = 'Roasts';
-    }
-    elseif($product[3] == 71){
-        $marketPhrase = 'Classic Goodness!';
-        $altPhrase = 'Classic Goodness!';
-        $titlePhrase = 'Classic Goodness!';
-        $productTitle = 'T-Bone Steaks';
-    }
-    else{
+ 
         $marketPhrase = '';
         $altPhrase = '';
         $titlePhrase = '';
         $productTitle = $formattedTitle;
-    }
+ 
     
     ?>
     
